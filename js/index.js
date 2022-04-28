03ed6c80386eb7ca693f240eb0834c3989d50e70
NguyetNguuyen <moonlight176kun@gmail.com>
2022-04-29 01:28:42 +0700

    ASM4: Home - Reponsive Menu

--- js/index.js
+++ js/index.js
@@ -1,8 +1,28 @@
-function myFunction() {
+document.addEventListener("click", function (e) {
+    var menu = document.getElementById('menu');
+    var menuIcon = document.getElementById('icon');
+
+    console.log("Click document");
+    var isClickOutsideMenu = !menu.contains(e.target);
+    var isClickOutsideIcon = !menuIcon.contains(e.target);
+    if (isClickOutsideMenu && isClickOutsideIcon && menu.className === "topnav-responsive") {
+        //Do something click is outside specified element
+        console.log(menu.className);
+        menu.className = "topnav";
+    } 
+}
+)
+
+function onClickMenuIcon() {
     var x = document.getElementById("menu");
+    console.log("onClickMenuIcon " + x.className);
     if (x.className === "topnav") {
+        console.log("onClickMenuIcon 1");
         x.className = "topnav-responsive";
     } else {
         x.className = "topnav";
+        console.log("onClickMenuIcon 2");
     }
 }
+
+
